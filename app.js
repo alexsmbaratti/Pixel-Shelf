0e05c34ee8aaa9df1d3874d4c4ee423f6ce95330
Alex Baratti <alexsmbaratti@gmail.com>
2021-06-01 21:56:43 -0700

    Boilerplate for add

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var libraryRouter = require('./routes/library');
+var addGameRouter = require('./routes/add');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/library', libraryRouter);
+app.use('/add', addGameRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
