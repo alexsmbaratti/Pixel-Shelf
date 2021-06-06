034dbf3a5e819d40c192756e5e6f73cfc33f8175
Alex Baratti <alexsmbaratti@gmail.com>
2021-06-05 22:16:20 -0700

    Boilerplate for mass import

--- public/js/mass.js
+++ public/js/mass.js
@@ -2,3 +2,35 @@ function append(text) {
     let schemaField = document.getElementById("schema");
     schemaField.value += text;
 }
+
+function submit() {
+    if (validateFields()) {
+        let schemaText = document.getElementById("schema").value;
+        let massText = document.getElementById("mass-text").value;
+
+        let request = new XMLHttpRequest();
+        request.open('POST', `/add`);
+        request.setRequestHeader('Content-Type', 'application/json');
+
+        let params = {
+
+        };
+
+        request.onreadystatechange = function () {
+            if (request.readyState === 4) {
+                let data = JSON.parse(request.responseText);
+                if (request.status === 200) {
+                    window.location.href = `/library/`;
+                } else {
+                    console.log("TODO: Handle error!")
+                }
+            }
+        }
+
+        request.send(JSON.stringify(params));
+    }
+}
+
+function validateFields() {
+    return true; // TODO
+}
