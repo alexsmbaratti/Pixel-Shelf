a06d2566b65c70e75aa1441490c77c670ae35248
Alex Baratti <alexsmbaratti@gmail.com>
2021-06-06 21:11:57 -0700

    Boilerplate for sorting by attribute

---
+++ public/js/library.js
@@ -0,0 +1,16 @@
+function sortBy(sortCriteria) {
+    switch (sortCriteria) {
+        case "title":
+            break;
+        case "platform":
+            break;
+        case "dateAdded":
+            break;
+        case "cost":
+            break;
+        case "edition":
+            break;
+        default:
+
+    }
+}
