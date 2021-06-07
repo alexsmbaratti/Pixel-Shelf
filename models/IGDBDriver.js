daf7e76dea1447887bfcf4c3dfd3d3b1854697a8
Alex Baratti <alexsmbaratti@gmail.com>
2021-06-06 21:24:09 -0700

    Adds boilerplate for IGDBDriver

---
+++ models/IGDBDriver.js
@@ -0,0 +1,8 @@
+const axios = require('axios');
+var config = require('../config.json');
+
+function IGDBDriver() {
+    IGDBDriver.prototype.clientID = config.client_id;
+    IGDBDriver.prototype.clientSecret = config.client_secret;
+    IGDBDriver.prototype.token = config.token;
+}
