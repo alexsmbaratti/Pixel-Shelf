27e5cb0ab2035f42b5a22ab5f4466e464f13fae3
Alex Baratti <alexsmbaratti@gmail.com>
2021-06-09 22:05:15 -0700

    Boilerplate for getCoverArtByID

--- models/IGDBDriver.js
+++ models/IGDBDriver.js
@@ -30,4 +30,30 @@ IGDBDriver.prototype.getGameByURL = function getGameByURL(url) {
     });
 }
 
+IGDBDriver.prototype.getCoverArtByID = function getCoverArtByID(id) {
+    // TODO: TEST! This is still boilerplate!
+    return new Promise(function (resolve, reject) {
+        axios({
+            method: 'post',
+            url: 'https://api.igdb.com/v4/covers',
+            headers: {
+                'Client-ID': IGDBDriver.prototype.clientID,
+                'Authorization': 'Bearer ' + IGDBDriver.prototype.token,
+                'Content-Type': 'text/plain'
+            },
+            data: 'fields image_id; where game = \"' + id + '\";'
+        })
+            .then(function (res) {
+                let size = 'cover_big';
+                let imageID = res.image_id;
+                let url = `https://images.igdb.com/igdb/image/upload/t_${size}/${imageID}.jpg`
+                resolve(url);
+            })
+            .catch(function (e) {
+                console.log(e);
+                reject(e);
+            });
+    });
+}
+
 module.exports = IGDBDriver;
