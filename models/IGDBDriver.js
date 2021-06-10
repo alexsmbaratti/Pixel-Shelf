d97b1d0ddcd7f892465b51a290f9b3f815c4899e
Alex Baratti <alexsmbaratti@gmail.com>
2021-06-09 21:50:32 -0700

    Puts in getGameByURL

--- models/IGDBDriver.js
+++ models/IGDBDriver.js
@@ -6,3 +6,28 @@ function IGDBDriver() {
     IGDBDriver.prototype.clientSecret = config.client_secret;
     IGDBDriver.prototype.token = config.token;
 }
+
+IGDBDriver.prototype.getGameByURL = function getGameByURL(url) {
+    return new Promise(function (resolve, reject) {
+        axios({
+            method: 'post',
+            url: 'https://api.igdb.com/v4/games/',
+            headers: {
+                'Client-ID': IGDBDriver.prototype.clientID,
+                'Authorization': 'Bearer ' + IGDBDriver.prototype.token,
+                'Content-Type': 'text/plain'
+            },
+            data: 'fields *; where url = \"' + url + '\";'
+        })
+            .then(function (res) {
+                console.log(JSON.stringify(res.data));
+                resolve(res);
+            })
+            .catch(function (e) {
+                console.log(e);
+                reject(e);
+            });
+    });
+}
+
+module.exports = IGDBDriver;
