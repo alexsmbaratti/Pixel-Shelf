bb56be21b1bc9051f055d87ab327d264e79d2ecd
Alex Baratti <alexsmbaratti@gmail.com>
2021-06-12 19:57:17 -0700

    Gets igdb info on page load

--- public/js/libraryEntry.js
+++ public/js/libraryEntry.js
@@ -1,5 +1,22 @@
-function deleteGame(id) {
+function getIGDBInfo(id) {
+    let igdbRequest = new XMLHttpRequest();
+    igdbRequest.open('GET', `/library/${id}/igdb`);
 
+    igdbRequest.onreadystatechange = function () {
+        if (igdbRequest.readyState === 4) {
+            let data = JSON.parse(igdbRequest.responseText);
+            if (igdbRequest.status === 200) {
+                console.log(data);
+            } else {
+                console.log("TODO: Handle error!")
+            }
+        }
+    }
+
+    igdbRequest.send();
+}
+
+function deleteGame(id) {
     let request = new XMLHttpRequest();
     request.open('DELETE', `/library/${id}`);
     request.setRequestHeader('Content-Type', 'application/json');
