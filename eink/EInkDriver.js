dab44c15b34305feb296f0b5731767b5ae4c2c36
Alex Baratti <alexsmbaratti@gmail.com>
2021-06-19 11:34:44 -0700

    Creates promise

--- eink/EInkDriver.js
+++ eink/EInkDriver.js
@@ -6,15 +6,17 @@ function EInkDriver() {
 }
 
 EInkDriver.prototype.drawLibrarySize = function drawLibrarySize() {
-    if (EInkDriver.prototype.useEInk) {
-        const python = spawn('python3', ['./eink/library_size.py', -1]);
-        python.stdout.on('data', function (data) {
-            console.log(data.toString());
-        });
-        python.on('close', (code) => {
-            console.log("Existed with code " + code);
-        });
-    }
+    new Promise(function (resolve, reject) {
+        if (EInkDriver.prototype.useEInk) {
+            const python = spawn('python3', ['./eink/library_size.py', -1]);
+            python.stdout.on('data', function (data) {
+                console.log(data.toString());
+            });
+            python.on('close', (code) => {
+                console.log("Existed with code " + code);
+            });
+        }
+    });
 }
 
 module.exports = EInkDriver;
