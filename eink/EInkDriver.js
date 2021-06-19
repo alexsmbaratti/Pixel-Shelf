5b96324e55777aaabfa87a4f7f7b7e7da825ad9b
Alex Baratti <alexsmbaratti@gmail.com>
2021-06-19 12:12:04 -0700

    Queries library size

--- eink/EInkDriver.js
+++ eink/EInkDriver.js
@@ -1,20 +1,28 @@
 var use = require('../config.json')['e-ink'];
 const PythonShell = require('python-shell').PythonShell;
+var SQLite3Driver = require('../models/SQLite3Driver');
 
 function EInkDriver() {
+    EInkDriver.prototype.driver = new SQLite3Driver();
 }
 
 EInkDriver.prototype.drawLibrarySize = function drawLibrarySize() {
     new Promise(function (resolve, reject) {
         if (use) {
             console.log("Drawing library size...");
-            let options = {
-                mode: 'text',
-                args: ['-1']
-            };
-            PythonShell.run('eink/library_size.py', options, function (err, res) {
-		if (err) { throw err; }
-                console.log(res);
+            EInkDriver.prototype.driver.getLibrarySize().then(result => {
+                let options = {
+                    mode: 'text',
+                    args: [result]
+                };
+                PythonShell.run('eink/library_size.py', options, function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log(res);
+                });
+            }).catch(err => {
+                console.log(err);
             });
         }
     });
