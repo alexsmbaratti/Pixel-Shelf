23cabcb26cd5651a109cfc2c9aa74de181ce833b
Alex Baratti <alexsmbaratti@gmail.com>
2021-06-18 21:41:28 -0700

    Ability to call on Python3 script

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const {spawn} = require('child_process');
+const useEInk = require('config.json')["e-ink"];
+const eInkPath = require('config.json')["e-ink-path"];
 
 var indexRouter = require('./routes/index');
 var libraryRouter = require('./routes/library');
@@ -18,7 +21,7 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -29,19 +32,24 @@ app.use('/identify', identifyRouter);
 app.use('/platforms', platformsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
+
+if (useEInk) {
+    console.log("Spawning E-Ink Driver...");
+    const python = spawn('python3', [eInkPath]);
+}
 
 module.exports = app;
