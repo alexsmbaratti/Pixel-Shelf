1bf1ca611fc0e5d0cb20c56e95f08f667bbb0317
Alex Baratti <alexsmbaratti@gmail.com>
2021-06-19 19:22:30 -0700

    Updates display after a game is added

--- routes/add.js
+++ routes/add.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var SQLite3Driver = require('../models/SQLite3Driver');
+var EInkDriver = require('../eink/EInkDriver');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -19,6 +20,8 @@ router.post('/', function (req, res) {
     driver.addGame(req.body).then(result => {
         if (result != undefined) {
             res.status(200).send({"status": 200, "id": result});
+            let eInkDriver = new EInkDriver();
+            eInkDriver.drawLibrarySize();
         } else {
             res.status(500).send({"status": 500});
         }
@@ -35,6 +38,8 @@ router.post('/mass', function (req, res) {
     let driver = new SQLite3Driver();
     driver.massImport(req.body).then(result => {
         res.status(200).send({"status": 200});
+        let eInkDriver = new EInkDriver();
+        eInkDriver.drawLibrarySize();
     }).catch(err => {
         res.status(500).send({"status": 500});
     });
