94caae54d269f0fb55dd3bf234c1bbfc12b8f78c
Alex Baratti <alexsmbaratti@gmail.com>
2021-06-20 12:39:05 -0700

    Base for backend info

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const version = require('../package.json').version;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Pixel Shelf' });
+});
+
+router.get('/backend', function(req, res, next) {
+  res.render('backend', { title: 'Pixel Shelf', version: version });
 });
 
 module.exports = router;
