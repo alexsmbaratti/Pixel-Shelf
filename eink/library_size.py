a1c06d1c89971973f7062538821a5f9235b2eb2e
Alex Baratti <alexsmbaratti@gmail.com>
2021-07-03 22:15:30 -0700

    Testing using preformatted screen

--- eink/library_size.py
+++ eink/library_size.py
@@ -12,11 +12,6 @@ dc = digitalio.DigitalInOut(board.D22)
 rst = digitalio.DigitalInOut(board.D27)
 busy = digitalio.DigitalInOut(board.D17)
 
-# Logo setup
-logo = Image.open("logo.png") # This should be a 1x1 image for proper scaling
-logo = logo.resize((11, 11), Image.BICUBIC)
-logo = logo.crop((5, 25, 127, 275)).convert("RGB")
-
 small_font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 10)
 medium_font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 20)
 large_font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 36)
@@ -29,10 +24,10 @@ display = Adafruit_SSD1680(122, 250, spi, cs_pin=ecs, dc_pin=dc, sramcs_pin=None
 display.rotation = 1
 
 display.fill(Adafruit_EPD.WHITE)
-image = Image.new("RGB", (display.width, display.height), color=WHITE)
+image = Image.open("logo.png")
 draw = ImageDraw.Draw(image)
 draw.text((5, 5), 'My Game Collection', font=medium_font, fill=BLACK,)
-draw.text((5, 112), 'Powered by Pixel Shelf', font=small_font, fill=BLACK,)
+draw.text((16, 112), 'Powered by Pixel Shelf', font=small_font, fill=BLACK,)
 draw.text((5, 25), sys.argv[1] + ' Games',font=large_font,fill=BLACK,)
 
 display.image(image)
