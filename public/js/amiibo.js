e4b600a135f429828da2ce98ca400881b311b206
Alex Baratti <alexsmbaratti@gmail.com>
2021-07-07 21:09:49 -0700

    amiibo api endpoint stub added

---
+++ public/js/amiibo.js
@@ -0,0 +1,25 @@
+function fetchAmiibo(sortBy = "title") {
+    let request = new XMLHttpRequest();
+    request.open('GET', `/api/amiibo?sortBy=${sortBy}`);
+
+    request.onreadystatechange = function () {
+        if (request.readyState === 4) {
+            let data = JSON.parse(request.responseText);
+            let mainDiv = document.getElementById("main-div");
+            while (mainDiv.firstChild) {
+                mainDiv.removeChild(mainDiv.firstChild);
+            }
+
+            if (request.status === 200) {
+
+            } else {
+                let noAmiiboText = document.createElement("p");
+                noAmiiboText.setAttribute("class", "title has-text-centered");
+                noAmiiboText.innerHTML = "An error has occurred!";
+                mainDiv.appendChild(noAmiiboText);
+            }
+        }
+    }
+
+    request.send();
+}
--- routes/api.js
+++ routes/api.js
@@ -72,6 +72,10 @@ router.put('/library/:libraryId/progress', function (req, res, next) {
     res.status(501).send({"status": 501, "msg": "Not Implemented!"});
 });
 
+router.get('/amiibo', function (req, res, next) {
+    res.status(501).send({"status": 501, "msg": "Not Implemented!"});
+});
+
 router.delete('/library/:libraryId', function (req, res, next) {
     let driver = new SQLite3Driver();
     driver.deleteGame(req.params.libraryId).then(result => {
