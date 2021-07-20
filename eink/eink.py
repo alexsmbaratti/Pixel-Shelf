2d15d179b48c8f5e7ec910c1f82a326e85d1e119
Alex Baratti <alexsmbaratti@gmail.com>
2021-07-19 18:23:29 -0700

    Testing switches

--- eink/eink.py
+++ eink/eink.py
@@ -24,6 +24,10 @@ ecs = digitalio.DigitalInOut(board.CE0)
 dc = digitalio.DigitalInOut(board.D22)
 rst = digitalio.DigitalInOut(board.D27)
 busy = digitalio.DigitalInOut(board.D17)
+switch1 = DigitalInOut(board.D6)
+switch2 = DigitalInOut(board.D5)
+switch1.direction = Direction.INPUT
+switch2.direction = Direction.INPUT
 
 display = Adafruit_SSD1680(122, 250, spi, cs_pin=ecs, dc_pin=dc, sramcs_pin=None, rst_pin=rst, busy_pin=busy, )
 
@@ -62,4 +66,8 @@ schedule.every(10).minutes.do(update)
 
 while True:
     schedule.run_pending()
-    time.sleep(1)
+    if not switch1.value:
+        print("Switch 1")
+    if not switch2.value:
+        print("Switch 2")
+    time.sleep(0.01)
