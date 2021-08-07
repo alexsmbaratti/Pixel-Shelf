13ecc48265d10377f02b5283aaab6c1ac62bcf17
Alex Baratti <alexsmbaratti@gmail.com>
2021-08-07 15:31:45 -0700

    Handles 200 on frontend for retailers

--- public/js/retailers.js
+++ public/js/retailers.js
@@ -11,7 +11,32 @@ function fetchRetailers(sortBy = "retailer") {
             }
 
             if (request.status === 200) {
+                data = data['data'];
 
+                if (data.length == 0) {
+                    let noRetailerText = document.createElement("p");
+                    noRetailerText.setAttribute("class", "title has-text-centered");
+                    noRetailerText.innerHTML = "No Retailers to Display...";
+                    mainDiv.appendChild(noRetailerText);
+                    return;
+                }
+
+                let tableBody = document.getElementById("table-body");
+                while (tableBody.firstChild) {
+                    tableBody.removeChild(tableBody.firstChild);
+                }
+
+                data.forEach(retailer => {
+                    let link = document.createElement("a");
+                    link.setAttribute("href", `/retailer/${retailer.id}`);
+                    let title = document.createElement("th");
+                    link.innerHTML = retailer.title;
+                    title.appendChild(link);
+
+                    let row = document.createElement("tr");
+                    row.appendChild(title);
+                    tableBody.appendChild(row);
+                });
             } else if (request.status === 501) {
                 let noRetailerText = document.createElement("p");
                 noRetailerText.setAttribute("class", "title has-text-centered");
