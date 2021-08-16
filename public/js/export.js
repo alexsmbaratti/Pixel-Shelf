5c2d8501eb5c1046988d94af653b40de787a62c0
Alex Baratti <alexsmbaratti@gmail.com>
2021-08-15 21:49:47 -0700

    Adds Apple Maps endpoint checker for backend info

--- public/js/export.js
+++ public/js/export.js
@@ -1,6 +1,5 @@
 function requestBackup() {
     let button = document.getElementById("submit");
-    button.setAttribute("class", "button is-link is-loading");
     button.disabled = true;
 
     window.open('/api/export');
