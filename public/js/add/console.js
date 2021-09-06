823797e7430ddc459b88c69d5448e4d4bb190c35
Alex Baratti <alexsmbaratti@gmail.com>
2021-09-05 17:53:59 -0700

    Moves swap functions from add console into utils.js

--- public/js/add/console.js
+++ public/js/add/console.js
@@ -55,13 +55,3 @@ function submitConsoleInfo() {
         "brand": brandText
     }));
 }
-
-function swapDiv(oldDivID, newDivID) {
-    document.getElementById(oldDivID).classList.add("is-hidden");
-    document.getElementById(newDivID).classList.remove("is-hidden");
-}
-
-function swapStepsProgress(oldStepID, newStepID) {
-    document.getElementById(oldStepID).classList.remove("is-active");
-    document.getElementById(newStepID).classList.add("is-active");
-}
