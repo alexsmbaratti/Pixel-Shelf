7635a7ad7d3b2c011421aca0e378f9a23c31c334
Alex Baratti <alexsmbaratti@gmail.com>
2021-09-05 17:55:05 -0700

    Moves swap functions from add retailer into utils.js

--- public/js/add/retailer.js
+++ public/js/add/retailer.js
@@ -96,13 +96,3 @@ function toggleOnline(isOnline) {
         onlineDiv.setAttribute('class', '');
     }
 }
-
-function swapDiv(oldDivID, newDivID) {
-    document.getElementById(oldDivID).classList.add("is-hidden");
-    document.getElementById(newDivID).classList.remove("is-hidden");
-}
-
-function swapStepsProgress(oldStepID, newStepID) {
-    document.getElementById(oldStepID).classList.remove("is-active");
-    document.getElementById(newStepID).classList.add("is-active");
-}
