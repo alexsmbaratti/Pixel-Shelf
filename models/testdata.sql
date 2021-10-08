c33780e467c9f1b82e6c0a980aa6d5e11c257edf
Alex Baratti <alexsmbaratti@gmail.com>
2021-10-08 16:06:30 -0700

    Creates, but does not implement, game library table mapping

--- models/testdata.sql
+++ models/testdata.sql
@@ -4,6 +4,9 @@ VALUES ('A Brand', 1);
 INSERT INTO platform
 VALUES ('A Console', 1, 1);
 
+INSERT INTO retailer
+VALUES ('A Retailer', 'Anytown Location', 0, 30.000, -70.000, NULL, 1);
+
 INSERT INTO game
 VALUES ('A Game', 1, NULL, 1);
 
@@ -11,4 +14,4 @@ INSERT INTO edition
 VALUES ('Standard Edition', NULL, 59.99, 1, 0, 'USD', 1, 1);
 
 INSERT INTO library
-VALUES (39.99, NULL, 1, NULL, 1, 1, 1, 3, 0, 'USD', 0, NULL, 1);
+VALUES (39.99, NULL, 1, 1, 1, 1, 1, 3, 0, 'USD', 0, NULL, 1);
--- test/client.test.js
+++ test/client.test.js
@@ -23,3 +23,13 @@ test('Error 404 Loads', () => {
         .get("/obviously-not-a-page")
         .expect(404);
 });
+
+test('Game Library Table Mapping', () => {
+    return supertest(pixelShelf)
+        .get("/library")
+        .expect(200)
+        .then((response) => {
+            console.log(response.text);
+            // TODO: Check each row of the table and compare with expected results
+        });
+});
